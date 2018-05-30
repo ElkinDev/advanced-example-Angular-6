efdc382467a36a3201028e0844753a23a766a856
ElkinDev <elkinmendoza00@gmail.com>
2018-05-30 16:49:52 -0500

    Creando Directivas y haciendo uso del @Hostlistener con @angular-cli

--- src/app/shadow-title.directive.ts
+++ src/app/shadow-title.directive.ts
@@ -8,17 +8,26 @@ export class ShadowTitleDirective {
   constructor(element:ElementRef) {
     element.nativeElement.style.textShadow='2px 2px 2px #19967E,3px 3px 2px #19967E,4px 4px 2px #19967E';
    }
-   @HostListener('click') doSomething(){
-     console.log('Clicked')
+  //  @HostListener('click') doSomething(){
+  //    console.log('Clicked')
+  //  }
+
+  //  @HostListener('mouseenter') doSomethingElse(){
+  //   console.log('Enter Mouse')
+  // }
+  // @HostListener('mouseleave') doSomethingElseElse(){
+  //   console.log('Leave Mouse')
+  // }
+  // @HostListener('mousemove') doSomethingElseElseElse(){
+  //   console.log('Move Mouse')
+  // }
+  //  @HostListener('document:click',['$event'])
+  //  elementCliked(elem){
+  //    console.log('clicked',elem)
+  //  }
+   @HostListener('click',['$event'])
+   elementCliked(elem){
+     console.log('clicked',elem)
    }
 
-   @HostListener('mouseenter') doSomethingElse(){
-    console.log('Enter Mouse')
-  }
-  @HostListener('mouseleave') doSomethingElseElse(){
-    console.log('Leave Mouse')
-  }
-  @HostListener('mousemove') doSomethingElseElseElse(){
-    console.log('Move Mouse')
-  }
 }
